6d61e70c37cb545045cf25057ca51937b7762ef9
Maurício Mafra Monnerat <monnerat.mauricio@gmail.com>
2025-07-17 03:40:36 -0300

    feat: draw orange triangle

--- src/nebula.cpp
+++ src/nebula.cpp
@@ -1,9 +1,24 @@
 #include <SDL3/SDL.h>
 #include <glad/glad.h>
+#include <iostream>
 
-void cleanup(SDL_Window *window);
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main() {\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main() {\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" //orange-ish color
+                                   "}\0";
+
 void setGLContextAttributes();
 void setGLBufferAttributes();
+void getGLVersionInfo();
+unsigned int setupShaders(SDL_Window *window);
+void cleanup(SDL_Window *window);
 
 // cmake . -B build
 // cmake --build build
@@ -36,8 +51,39 @@ int main() {
 
     if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing GLAD", nullptr);
+        cleanup(window);
         return 1;
     }
+
+    getGLVersionInfo();
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int shaderProgram = setupShaders(window);
+    if (!shaderProgram) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error compilating vertex shader", nullptr);
+        cleanup(window);
+        return 1;
+    }
+
+    unsigned int vao {};
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    unsigned int vbo {};
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     bool run = true;
     while (run) {
@@ -60,10 +106,17 @@ int main() {
         // (state-using function) fills the color buffer with the color configured by glClearColor
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // swap buffers
         SDL_GL_SwapWindow(window);
     }
     
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
     cleanup(window);
     return 0;
 }
@@ -96,8 +149,57 @@ void setGLBufferAttributes() {
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 }
 
+void getGLVersionInfo() {
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
+    std::cout << "Version: " << glGetString(GL_VERSION) << "\n";
+    std::cout << "Shading Language Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+}
+
+unsigned int setupShaders(SDL_Window *window) {
+    int success {};
+    char infoLog[512] {};
+
+    unsigned int vertexShader {};
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        SDL_Log("%s,%s","ERROR::SHADER::VERTEX::COMPILATION_FAILED", infoLog);
+        return success;
+    }
+
+    unsigned int fragmentShader {};
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        SDL_Log("%s,%s","ERROR::SHADER::FRAGMENT::COMPILATION_FAILED", infoLog);
+        return success;
+    }
+
+    unsigned int shaderProgram {};
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        SDL_Log("%s,%s","ERROR::SHADER::PROGRAM::COMPILATION_FAILED", infoLog);
+        return success;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderProgram;
+}
+
 void cleanup(SDL_Window *window) {
-    SDL_Log("%s", "Cleanup");
+    SDL_Log("%s", "CLEANUP");
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
